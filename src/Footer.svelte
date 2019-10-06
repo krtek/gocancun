a2d2f09efa6ef7b2380f2d8aaf69d3f8460b38c8
Lukáš Marek <lukas_marek@kb.cz>
2019-10-06 12:41:01 +0200

    🐛Full width footer
    Even on bigscreen devices.

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <style lang="scss">
-  .container {
+  .container-fluid {
     background-color: black;
   }
 </style>
 
 <footer class="footer mt-auto py-0">
-  <div class="container pl-4">
+  <div class="container-fluid pl-4">
     {#if photo}
     <span class="text-white text-center">📸 by <a href="{photo.links.html}" target="_blank">{photo.user.first_name} {photo.user.last_name}</a></span>
     {/if}
